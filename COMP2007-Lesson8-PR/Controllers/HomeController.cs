9f4d25d9496a20fbd7bf99c7ca2bfe585ced2306
patr9240 <patr9240@gmail.com>
2016-07-15 20:43:14 -0400

    Added courses, Students, and Departments list pages.

--- COMP2007-Lesson8-PR/Controllers/HomeController.cs
+++ COMP2007-Lesson8-PR/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ namespace COMP2007_Lesson8_PR.Controllers
 {
     public class HomeController : Controller
     {
+
+        //GET: /Home/Index or /
         public ActionResult Index()
         {
             return View();
         }
 
+        //GET: /Home/About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +23,7 @@ namespace COMP2007_Lesson8_PR.Controllers
             return View();
         }
 
+        //GET: /Home/Contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
